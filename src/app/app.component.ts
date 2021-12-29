c30dd477c66ed674d8931208d4c1ea14e9a2ad97
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-29 16:20:24 -0300

    Desafio Teste Accurate Dev Front End - <Matheus Magalhães>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'accurate-software';
+constructor(private http: HttpClient) {}
+
+  emailFinder: any = {};
+
+  isOpen: boolean = false; //click de botão
+
+
+//Metodo chama o Api
+loadHome(){
+
+  this.isOpen = !this.isOpen; //click de botão
+
+  this.http
+  .get('https://api.hunter.io/v2/email-finder?domain=reddit.com&first_name=Alexis&last_name=Ohanian&api_key=b740a24081e7458eed4aa7e6a65b3d4a5c5469ce')
+  .subscribe((emailFinder: any[])=>{
+    this.emailFinder = emailFinder;
+
+    console.log(this.emailFinder);
+
+  });
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http' //1° import pacote angular
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
